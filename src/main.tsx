808ae2655c0264ebd888ab92368091ab583d5c95
João Álvaro <joaosilvaabc1@gmail.com>
2025-11-02 00:05:55 -0300

    ✨ feat: Adição de rotas react

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import HomeLayout from './components/HomeLayout.tsx';
+import AeronaveLayout from './components/AeronaveLayout.tsx';
+
+import DashboardAeronaves from './pages/DashboardAeronaves.tsx';
+
+
+const router = createBrowserRouter([
+  { path: '*', element: <div>404 Not Found</div> },
+  // { path: '/', element: <Login /> },
+  { 
+    path: '/home',
+    element: <HomeLayout />,
+    children: [
+      { index: true, element: <DashboardAeronaves />},
+    ]
+  },
+  {
+    path: '/aeronave/:aeronaveId',
+    element: <AeronaveLayout />,
+    children: [
+      // { index: true, element: <GerenciaAeronave /> },
+      // { path: 'etapas', element: <GerenciaEtapa /> }
+    ]
+  }
+]);
+
+const root = document.getElementById('root');
+
+createRoot(root!).render(
+  <RouterProvider router={router} />
+);
